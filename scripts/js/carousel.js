d3a6f86c5aa9f1f9a7aef82080db165c74349c9c
MiniNinja2131 <dannyche@live.co.uk>
2021-04-23 18:43:10 +0100

    css refactoring

---
+++ scripts/js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#productImg").fancybox({
+        openEffect	: 'elastic',
+        closeEffect	: 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+});
